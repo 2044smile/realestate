3c2474d6c61f3885f8ff4b36a129078f27502669
2044smile <2044smile@naver.com>
2025-03-06 23:04:57 +0900

    feat: 매개변수 활용, default 값 사용

--- backend/realestate.py
+++ backend/realestate.py
@@ -10,17 +10,17 @@ from bs4 import BeautifulSoup
 
 
 class NaverLandCrawler:
-    def __init__(self, trad_tp_cds, rlet_tp_cds, tags, wprc_max, z, regions):
+    def __init__(self, regions, trad_tp_cds=None, rlet_tp_cds=None, tags=None, wprc_max=None, z=None):
         self.base_url = "https://m.land.naver.com"
         self.headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
         }
 
-        self.trad_tp_cds = trad_tp_cds # 거래 유형 코드 eg. "B1" 전세, "A1" 매매, "B2" 월세
-        self.rlet_tp_cds = rlet_tp_cds # 매물 유형 코드 eg. "DDDGG" 오피스텔, "VL" 빌라, "APT" 아파트
-        self.tags = tags # 특정 조건 태그 eg. "TWOROOM" 투룸 매물, "PARKINGYN" 주차 가능 매물
-        self.wprc_max = wprc_max # 최대 가격 eg. 30000 3억 원
-        self.z = z # 지도 줌 레벨 eg. "13" 구 단위, "14" 동
+        self.trad_tp_cds = trad_tp_cds or "B1" # 거래 유형 코드 eg. "B1" 전세, "A1" 매매, "B2" 월세
+        self.rlet_tp_cds = rlet_tp_cds or "DDDGG,VL" # 매물 유형 코드 eg. "DDDGG" 오피스텔, "VL" 빌라, "APT" 아파트
+        self.tags = tags or "TWOROOM,PARKINGYN" # 특정 조건 태그 eg. "TWOROOM" 투룸 매물, "PARKINGYN" 주차 가능 매물
+        self.wprc_max = wprc_max or 30000 # 최대 가격 eg. 30000 3억 원
+        self.z = z or 13 # 지도 줌 레벨 eg. "13" 구 단위, "14" 동
 
         self.regions = regions
         self.result_data = []
@@ -49,9 +49,9 @@ class NaverLandCrawler:
         """해당 지역의 매물 리스트 가져오기"""
         lat_margin, lon_margin = 0.118, 0.111
         btm, lft, top, rgt = float(lat) - lat_margin, float(lon) - lon_margin, float(lat) + lat_margin, float(lon) + lon_margin
-        trad_tp_cds, rlet_tp_cds, tags, wprc_max, z = "B1", "DDDGG,VL", "TWOROOM,PARKINGYN", 30000, 13
+        # trad_tp_cds, rlet_tp_cds, tags, wprc_max, z = "B1", "DDDGG,VL", "TWOROOM,PARKINGYN", 30000, 13
 
-        detail_url = f"{self.base_url}/cluster/clusterList?view=atcl&cortarNo={cortar_no}&tradTpCd={trad_tp_cds}&rletTpCd={rlet_tp_cds}&z={z}&lat={lat}&lon={lon}&tag={tags}&btm={btm}&lft={lft}&top={top}&rgt={rgt}&wprcMax={wprc_max}"
+        detail_url = f"{self.base_url}/cluster/clusterList?view=atcl&cortarNo={cortar_no}&tradTpCd={self.trad_tp_cds}&rletTpCd={self.rlet_tp_cds}&z={self.z}&lat={lat}&lon={lon}&tag={self.tags}&btm={btm}&lft={lft}&top={top}&rgt={rgt}&wprcMax={self.wprc_max}"
         response = requests.get(detail_url, headers=self.headers)
         
         if response.status_code != 200:
@@ -67,7 +67,7 @@ class NaverLandCrawler:
 
     def fetch_articles(self, listings, cortar_no, region_name):
         """개별 매물 상세 정보 가져오기"""
-        trad_tp_cds, rlet_tp_cds, tags, wprc_max, z = "B1", "DDDGG,VL", "TWOROOM,PARKINGYN", 30000, 13
+        # trad_tp_cds, rlet_tp_cds, tags, wprc_max, z = "B1", "DDDGG,VL", "TWOROOM,PARKINGYN", 30000, 13
 
         for listing in listings:
             lgeo, count, lat2, lon2 = listing['lgeo'], listing['count'], listing['lat'], listing['lon']
@@ -75,7 +75,7 @@ class NaverLandCrawler:
 
             for idx in range(1, len_pages + 1):
                 print(f"[{region_name}] 매물 {lgeo} - 페이지 {idx}")
-                list_url = f"{self.base_url}/cluster/ajax/articleList?itemId={lgeo}&lgeo={lgeo}&tradTpCd={trad_tp_cds}&rletTpCd={rlet_tp_cds}&tag={tags}&z={z}&lat={lat2}&lon={lon2}&totCnt={count}&cortarNo={cortar_no}&page={idx}&wprcMax={wprc_max}"
+                list_url = f"{self.base_url}/cluster/ajax/articleList?itemId={lgeo}&lgeo={lgeo}&tradTpCd={self.trad_tp_cds}&rletTpCd={self.rlet_tp_cds}&tag={self.tags}&z={self.z}&lat={lat2}&lon={lon2}&totCnt={count}&cortarNo={cortar_no}&page={idx}&wprcMax={self.wprc_max}"
                 
                 time.sleep(random.uniform(1, 3))
                 response = requests.get(list_url, headers=self.headers)
@@ -124,7 +124,7 @@ class NaverLandCrawler:
             'atcl_price': " ".join(price_text.split(' ')[1:]),
             'atcl_cost': cost_text,
             'atcl_description': description_text,
-            'atcl_location_': location_url
+            'atcl_location': location_url
         })
 
     def save_to_csv(self, region):
